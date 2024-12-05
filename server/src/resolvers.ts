a5f92736887c84fd00391323570e3138a17d6f67
kelumkp <kelumgit@outlook.com>
2024-12-06 11:31:54 +1300

    apollo certfied developer final part

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -17,6 +17,30 @@ export const resolvers: Resolvers = {
       return dataSources.trackAPI.getModule(id);
     },
   },
+  Mutation: {
+    //increment a track's numberOfViews property 
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try{  
+      const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+        return {
+          code : 200,
+          success: true,
+          message: `Successfully increment number of views for track ${id}`,
+          track
+        }
+      }
+      catch(err: any)
+      {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
